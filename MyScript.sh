144ce82ad84d28cb10f81812f037fb19231c2850
arindamchattopadhyay <40838263+arindamchattopadhyay@users.noreply.github.com>
2020-05-13 18:50:13 +0000

    Basics_1

---
+++ MyScript.sh
@@ -0,0 +1,17 @@
+echo "Number of files in the current directory"
+ls | wc -l
+echo "display day number in the year"
+cal -j
+echo "display current week number in a year"
+date +%u
+echo "display name of the processor"
+uname -p
+echo "display date in yyyy/mm/dd"
+date +%f
+echo "display domain name :-- "
+hostname -d
+echo "display IP address :-- "
+hostname -i
+
+
+
